f8bf6f4542b7a3ae654c50f956fc1cb37b0b7a93
Erick Vicente <vicente.erick@gmail.com>
2015-08-25 00:25:24 -0300

    Initial commit

---
+++ AudioEffects/RecordViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AudioEffects
+//
+//  Created by Erick Vicente on 24/08/15.
+//  Copyright (c) 2015 Erick Vicente. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: AnyObject) {
+        
+    }
+
+    @IBAction func stopRecordAudio(sender: AnyObject) {
+        
+    }
+    
+}
+
